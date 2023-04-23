782b916f847623d6ecb30dcc6e6b4ea6e0a09b80
Colinchang123 <130967210+Colinchang123@users.noreply.github.com>
2023-04-23 13:26:44 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,12 +4,12 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    System.out.println("Enter Weight in Kilograms:");
+
     
     Scanner in = new Scanner(System.in);
     double Weight = in.nextDouble();
     
-    System.out.println("Enter Height in Meters:");
+
     
     double height = in.nextDouble();
     
